ec8ae9d71d2260ef7b9f08466aaa92d7cf8c6847
jfabi88 <jacopo.fabi8@gmail.com>
2022-04-06 11:08:33 +0200

    jfabi 20220406 11:08

--- test/is_iterator_test.cpp
+++ test/is_iterator_test.cpp
@@ -2,10 +2,32 @@
 #include <vector>
 #include "../sources/iterator_traits.hpp"
 
+class my_class
+{
+    public:
+        typedef int value_type;
+        typedef int difference_type;
+        typedef int iterator_category;
+        typedef int pointer;
+        typedef int reference;
+};
+
 int main()
 {
     std::cout << std::boolalpha;
-    std::cout << "char is iterator: " <<ft::is_iterator<char>::value << std::endl;
-    std::cout << "vector.begin() is iterator: " <<ft::is_iterator<std::vector<int>::iterator>::value << std::endl;
+    std::cout << "   Is an iterator?" << std::endl;
+    std::cout << "char:                          " <<ft::is_iterator<char>::value << std::endl;
+    std::cout << "std::string:                   " <<ft::is_iterator<std::string>::value << std::endl;
+    std::cout << "int:                           " <<ft::is_iterator<int>::value << std::endl;
+    std::cout << "float:                         " <<ft::is_iterator<float>::value << std::endl;
+    std::cout << "int *:                         " <<ft::is_iterator<int*>::value << std::endl;
+    std::cout << "char*:                         " <<ft::is_iterator<char*>::value << std::endl;
+    std::cout << "vector<int>::iterator:         " <<ft::is_iterator<std::vector<int>::iterator>::value << std::endl;
+    std::cout << "vector<char>::iterator:        " <<ft::is_iterator<std::vector<char>::iterator>::value << std::endl;
+    std::cout << "vector<std::string>::iterator: " <<ft::is_iterator<std::vector<std::string>::iterator>::value << std::endl;
+    std::cout << "vector<int*>::iterator:        " <<ft::is_iterator<std::vector<int*>::iterator>::value << std::endl;
+    std::cout << "vector<float>::iterator:       " <<ft::is_iterator<std::vector<float>::iterator>::value << std::endl;
+    std::cout << "vector<float>:                 " <<ft::is_iterator<std::vector<float>>::value << std::endl;
+    std::cout << "my_class:                      " <<ft::is_iterator<my_class>::value << std::endl;
     return (0);
 }
